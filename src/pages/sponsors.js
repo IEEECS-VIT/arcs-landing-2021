b17ea13fd1108c313cc90bcf1bc178ee821e7bee
pragati2526 <spragati126@gmail.com>
2021-08-18 13:43:52 +0530

    responsive code updated

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,98 +1,104 @@
 import React from 'react'
-import image from '../images/sponsors_ball.png'
 
-import devf from '../images/devfo.png'
-import gfg from '../images/gfg.png'
-import jetb from '../images/jetb.png'
-import balsa from '../images/balsamiq.png'
-import matic from '../images/matic.png'
-import portis from '../images/portis.png'
+import image from '../images/sponsors/sponsors_ball.png'
+import combo from '../images/sponsors/combo2.png'
 
-import topleft from '../images/topleft.png'
-import topright from '../images/topright.png'
-import bottomleft from '../images/leftbottom.png'
-import bottomright1 from '../images/rightbottom1.png'
-import bottomright2 from '../images/rightbottom2.png'
+import devf from '../images/sponsors/devfo.png'
+import gfg from '../images/sponsors/gfg.png'
+import jetb from '../images/sponsors/jetb.png'
+import balsa from '../images/sponsors/balsamiq.png'
+import matic from '../images/sponsors/matic.png'
+import portis from '../images/sponsors/portis.png'
+
+import topleft from '../images/sponsors/topleft.png'
+import topright from '../images/sponsors/topright.png'
+import bottomleft from '../images/sponsors/leftbottom.png'
+import bottomright1 from '../images/sponsors/rightbottom1.png'
+import bottomright2 from '../images/sponsors/rightbottom2.png'
 
 export default function test() {
     return (
-        <div class="overflow-hidden test">
-            <div class=" grid grid-cols-5 grid-rows-4 gap-6 mt-24 justify-items-center">
-                <div class=" -mt-16 group row-span-2">
-                    <div class="z-40 -mb-24 -ml-10  transform group-hover:-translate-y-8 group-hover:translate-x-8 transition ease-in-out duration-700">
-                        <img class="object-scale-down w-20 h-20  " src={image} alt="image"/>
-                    </div>
-                    <div class="z-10 -ml-36 mt-16 transform  group-hover:scale-125 transition ease-in-out duration-500">
-                        <img class="object-scale-down w-52 h-52  " src={topleft} alt="image"/>
-                    </div>
-                </div>
-                <div class="col-span-3">
-                    <div class=" text-center text-6xl tit">
+        <div className="overflow-x-hidden object-fill">
+            <div className=" mt-4 sm:mt-10">
+                <div className="mx-auto">
+                {/* <div className="bg-no-repeat bgrd1" style={{background: {combo}}}/> */}
+                    <p className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl tit">
                         Sponsors
-                    </div>
-                </div>
-                <div class=" -mt-16 group row-span-2">
-                    <div class="z-40 -mb-24 -mr-10  transform group-hover:-translate-y-8 group-hover:-translate-x-8 transition ease-in-out duration-700">
-                            <img class="object-scale-down w-24 h-24" src={image} alt="image"/>
-                    </div>
-                    <div class="z-10 -mr-36 mt-16 transform  group-hover:translate-y-4 transition ease-in-out duration-500">
-                        <img class="object-scale-down w-52 h-52 " src={topright} alt="image"/>
-                    </div>
-                </div>
-                <div class=" col-span-3 row-span-2 -mt-64 grid grid-cols-1 grid-rows-6 md:grid-cols-3  sm:grid-cols-2 -m-24 gap-2 p-0 justify-items-center">
-                    
-                        {/* <div class="bg-no-repeat bgrd1" style={{backgroundImage: {image}}}> */}
-                        <div class="">
-                            <img class="object-scale-down w-24 h-24 md:w-40 md:h-40 sm:w-32 sm:h-32" src={devf} alt="image"/>
-                            <p class="text-xl text-center">Devfolio</p>
-                        </div>
-                        <div class="">
-                            <img class="object-scale-down w-24 h-24 md:w-40 md:h-40 sm:w-32 sm:h-32" src={gfg}alt="image" />
-                            <p class="text-xl text-center">Geek for Geeks</p>
-                        </div>
-                        <div class="">
-                            <img class="object-scale-down w-24 h-24 md:w-40 md:h-40 sm:w-32 sm:h-32" src={jetb}alt="image" />
-                            <p class="text-xl text-center">Jet Brais</p>
-                        </div>
-                        <div class="">
-                            <img class="object-scale-down w-24 h-24 md:w-40 md:h-40 sm:w-32 sm:h-32" src={balsa}alt="image" />
-                            <p class="text-xl text-center">Balsamiq</p>
-                        </div>
-                        <div class="">
-                            <img class="object-scale-down w-24 h-24 md:w-40 md:h-40 sm:w-32 sm:h-32" src={matic}alt="image" />
-                            <p class="text-xl text-center">Matic</p>
-                        </div>
-                        <div class="">
-                            <img class="object-scale-down w-24 h-24 md:w-40 md:h-40 sm:w-32 sm:h-32" src={portis}alt="image" />
-                            <p class="text-xl text-center">Portis</p>
-                        </div>
-                    
+                    </p>
                 </div>
 
-                <div class=" group row-span-2 -mt-72">
-                <div class="z-10 -mb-36 -ml-24  transform group-hover:-translate-y-24 group-hover:translate-x-10 transition ease-in-out duration-700">
-                            <img class="object-scale-down w-20 h-20" src={image} alt="image"/>
+                <div className=" group float-left">
+                    <div className="md:ml-12 md:mt-8 ml-4 sm:mt-12 sm:-translate-y-6 mt-8 transform  xs:group-hover:-translate-y-12 md:group-hover:-translate-y-8 sm:group-hover:translate-x-4 transition ease-in-out duration-700">
+                        <img className="object-scale-down scale-110 w-12 h-12 sm:w-18 sm:h-18 md:w-20 md:h-20" src={image} alt=""/>
                     </div>
-                    <div class="z-40 -mr-36 mt-16 -ml-16 transform  group-hover:scale-110 transition ease-in-out duration-500">
-                        <img class="object-scale-down w-52 h-52 " src={bottomleft} alt="image"/>
+                    <div className="mb-4 -ml-24 -mt-12 transform group-hover:scale-110 group-hover:translate-y-2 group-hover:translate-x-2 sm:group-hover:scale-125  transition ease-in-out duration-500">
+                        <img className="object-scale-down w-36 h-36 sm:w-44 sm:h-44 md:w-48 md:h-48" src={topleft} alt=""/>
                     </div>
                 </div>
 
-                <div class="group row-span-2 -mt-72">
-                    <div class="z-10 -mr-52 -mt-16 ml-8 transform  group-hover:-translate-y-8 group-hover:-translate-x-8 transition ease-in-out duration-700">
-                        <img class="object-scale-down w-52 h-52 " src={bottomright1} alt="image"/>
-                    </div>
-                    <div class="z-40 -mt-24 -mr-16  transform group-hover:translate-y-16  group-hover:translate-x-16 transition ease-in-out duration-700">
-                        <img class="object-scale-down w-20 h-20" src={image} alt="image"/>
-                    </div>
-                    <div class="z-20 -ml-96 -mt-4 transform  group-hover:translate-x-8 transition ease-in-out duration-700">
-                        <img class="object-scale-down " src={bottomright2} alt="image"/>
-                    </div>
-                </div>
-                <div class="group col-span-3">
-                </div>
 
-</div>
+                <div className="group float-right">
+                    <div className="z-40 transform xs:group-hover:-translate-y-4 md:group-hover:-translate-y-4 sm:group-hover:-translate-x-6 transition ease-in-out duration-700">
+                            <img className="object-scale-down w-12 h-12 md:w-24 md:h-24 sm:w-20 sm:h-20" src={image} alt=""/>
+                    </div>
+                    <div className="-mt-10 mb-4 -mr-32 transform group-hover:translate-y-2 transition ease-in-out duration-500">
+                        <img className="object-scale-down w-32 h-32 md:w-52 md:h-52 sm:w-44 sm:h-44 " src={topright} alt=""/>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className="mx-2 mt-16  sm:mt-32 md:mt-24  text-center place-items-center align-items-center justify-items-center xs:gap-2 gap-6 sm:gap-4 grid grid-cols-1  grid-rows-6 xs:grid-cols-2 xs:grid-rows-3 md:grid-cols-3  md:grid-rows-2 ">
+                    <div className="">
+                        <img className="mx-auto z-40 object-scale-down w-20 h-20 md:w-40 md:h-40 sm:w-28 sm:h-28" src={devf} alt=""/>
+                        <p className="text-l sm:text-xl text-center">Devfolio<br/></p>
+                    </div>
+                    <div className="">
+                        <img className="mx-auto z-40 object-scale-down w-20 h-20 md:w-40 md:h-40 sm:w-28 sm:h-28" src={gfg} alt=""/>
+                        <p className="text-l sm:text-xl text-center">Geek for Geeks</p>
+                    </div>
+                    <div className="">
+                        <img className="mx-auto z-40 object-scale-down w-20 h-20 md:w-40 md:h-40 sm:w-28 sm:h-28" src={jetb} alt=""/>
+                        <p className="text-l sm:text-xl text-center">Jet Brains<br/></p>
+                    </div>
+                    <div className="">
+                        <img className="mx-auto z-40 object-scale-down w-20 h-20 md:w-40 md:h-40 sm:w-28 sm:h-28" src={balsa} alt=""/>
+                        <p className="text-l sm:text-xl text-center">Balsamiq<br/></p>
+                    </div>
+                    <div className="">
+                        <img className="mx-auto z-40 object-scale-down w-20 h-20 md:w-40 md:h-40 sm:w-28 sm:h-28" src={matic} alt=""/>
+                        <p className="text-l sm:text-xl text-center">Matic<br/></p>
+                    </div>
+                    <div className="">
+                        <img className="mx-auto z-40 object-scale-down w-20 h-20 md:w-40 md:h-40 sm:w-28 sm:h-28" src={portis} alt=""/>
+                        <p className="text-l sm:text-xl text-center">Portis<br/></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="fixed bottom-0 left-0 right-0">
+            <div className=" group float-left">
+                    <div className="md:ml-12 md:mt-8 ml-4 sm:mt-12 mt-8 sm:translate-y-52 md:translate-y-80 xs:translate-y-40 transform group-hover:scale-110  xs:group-hover:translate-y-36 xs:group-hover:translate-x-8 sm:group-hover:translate-x-10 sm:group-hover:translate-y-40 md:group-hover:-translate-x-2 md:group-hover:translate-y-64 transition ease-in-out duration-700">
+                        <img className="object-scale-down scale-110 w-10 h-10 sm:w-18 sm:h-18 md:w-20 md:h-20" src={image} alt=""/>
+                    </div>
+                    <div className="ml-24 md:translate-y-72 sm:translate-y-52 sm:-translate-x-24 xs:translate-y-44 xs:-translate-x-12  bottom-0 transform group-hover:scale-110  sm:group-hover:-translate-x-20 md:group-hover:translate-x-0 md:group-hover:scale-110 transition ease-in-out duration-500">
+                        <img className="object-scale-down w-32 h-32 sm:w-44 sm:h-44 md:w-48 md:h-48" src={bottomleft} alt=""/>
+                    </div>
+            </div>
+
+
+                <div className="group float-right">
+                    <div className="z-40 md:translate-y-96 md:-translate-x-20 sm:translate-y-72 sm:-translate-x-8 xs:-translate-x-6 xs:translate-y-52 transform md:group-hover:translate-y-80 md:group-hover:-translate-x-6 sm:group-hover:translate-y-80 sm:group-hover:translate-x-0 xs:group-hover:translate-y-60 xs:group-hover:translate-x-2 transition ease-in-out duration-700">
+                            <img className="object-scale-down w-10 h-10 md:w-24 md:h-24 sm:w-20 sm:h-20" src={image} alt=""/>
+                    </div>
+                    <div className=" -mr-32 md:translate-y-40 md:-translate-x-4 sm:translate-y-16 sm:-translate-x-12 xs:translate-y-20 transform sm:group-hover:-translate-x-16 xs:group-hover:translate-y-10  sm:group-hover:translate-y-8 md:group-hover:translate-y-16 transition ease-in-out duration-500">
+                        <img className="object-scale-down w-32 h-32 md:w-52 md:h-52 sm:w-40 sm:h-40" src={bottomright1} alt=""/>
+                    </div>
+                    <div className=" -mr-32 md:translate-y-36 md:-translate-x-64 sm:-translate-x-40 sm:translate-y-24 xs:translate-y-24 xs:-translate-x-24 transform xs:group-hover:scale-110 sm:group-hover:-translate-x-36 md:group-hover:scale-125 md:group-hover:-translate-x-56 transition ease-in-out duration-500">
+                        <img className="object-scale-down w-32 h-32 md:w-52 md:h-52 sm:w-40 sm:h-40" src={bottomright2} alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
