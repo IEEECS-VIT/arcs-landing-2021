6769a6e1d395ac495d6a3deda2a4e28707cdd56d
[supraditya] <[aditya300100@gmail.com]>
2021-09-01 23:11:38 +0530

    title style rectified

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,9 +3,9 @@ import combo from '../images/sponsors/combo2.png'
 
 export default function test() {
     return (
-        <div className="w-full">
-            <div className="relative" style={{margin:"auto",border:"1px solid black"}}>
-                <img className="absolute "  src={combo} alt="lines"/>
+        <div className="w-full flex justify-center">
+            <div className="flex">
+                <img src={combo} alt="lines"/>
                 <p className=" text-8xl text-center absolute  mt-4 ml-16 mb-10">
                 Sponsors</p>
             </div>
