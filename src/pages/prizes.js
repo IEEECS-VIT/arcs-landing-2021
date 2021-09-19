6c90968f14f4d550f8a333d64b0c149b614661c3
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-09-19 21:34:50 +0530

    mobile view added

--- src/pages/prizes.js
+++ src/pages/prizes.js
@@ -2,29 +2,34 @@ import React, { useState } from "react";
 import Navbar from "../components/navbar";
 const Prizes = () => {
   const [isExpanded, toggleExpansion] = useState(false);
+  var Blur = isExpanded ? "change-opacity" : "none";
   return (
     <div className="-mt-5">
       <Navbar isExpanded={isExpanded} toggleExpansion={toggleExpansion} />
-      <p className=" text-center font-bold  text-5xl font-mont">PRIZES</p>
-      <div className="justify-center  mt-32 flex flex-row">
-        <p className="-mr-16 z-20 circles prize3 transform ease-in-out hover:z-50 duration-500 hover:scale-125">
-          <p className="text-center -mt-10 font-mont font-semibold text-2xl">
-            Quiz
-          </p>
+      <div className={Blur}>
+        <p className=" text-center font-bold text-4xl md:text-5xl font-mont">
+          PRIZES
         </p>
-        <hr className="-mr-3 rotate-90 border-2 h-80 " />
-        <p className="-mt-10 z-30 circles prize1  transform ease-in-out hover:z-50 duration-500 hover:scale-125">
-          <p className="text-center -mt-10 font-mont font-semibold text-2xl">
-            Competitive
+        <div className="justify-center mt-5 md:mt-32 flex flex-col md:flex-row">
+          <p className="md:-mr-16 z-20 circles prize3 transform ease-in-out  duration-500 hover:scale-125">
+            <p className="text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
+              Quiz
+            </p>
           </p>
-        </p>
-        <hr className=" -ml-3 z-10 rotate-90 border-2 h-80 " color="black" />
+          <hr className="-mr-3  lines md:rotate-90 border-2 md:h-80 " />
+          <p className="md:-mt-10 z-30 circles prize1  transform ease-in-out  duration-500 hover:scale-125">
+            <p className="text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
+              Competitive
+            </p>
+          </p>
+          <hr className=" -ml-3 z-10 md:rotate-90 border-2 md:h-80 lines " />
 
-        <p className="-ml-16 z-20 circles  prize2  transform ease-in-out hover:z-50 duration-500 hover:scale-125 ">
-          <p className="text-center -mt-10 font-mont font-semibold text-2xl">
-            Cicada
+          <p className="md:-ml-16 z-20 circles  prize2  transform ease-in-out  duration-500 hover:scale-125 ">
+            <p className="text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
+              Cicada
+            </p>
           </p>
-        </p>
+        </div>
       </div>
     </div>
   );
