b3d2507942200ccd787bc5e63d134dc4c5cab285
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-09-22 00:00:58 +0530

    Mobile view fixed

--- src/pages/prizes.js
+++ src/pages/prizes.js
@@ -10,13 +10,13 @@ const Prizes = () => {
         <p className=" text-center font-bold text-4xl md:text-5xl font-mont">
           PRIZES
         </p>
-        <div className="justify-center mt-3 md:mt-32 flex flex-col md:flex-row">
+        <div className="justify-center mt-7 md:mt-32 flex flex-col md:flex-row">
           <p className="md:-mr-16 z-10 circles prize3 transform ease-in-out  duration-500 hover:scale-125">
-            <p className="caption text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
+            <p className=" caption text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
               Quiz
             </p>
           </p>
-          <div className="z-30 border-r-2 border-l-2 border-black relative lines md:-mt-24">
+          <div className="z-30  md:border-r-4 md:border-l-4 border-black relative lines md:-mt-24">
             <p className="z-50 md:pb-10 relative caption-2 text-center  font-mont font-semibold text-2xl">
               Competitive
             </p>
@@ -24,7 +24,7 @@ const Prizes = () => {
           </div>
 
           <p className="md:-ml-16 z-10 circles  prize2  transform ease-in-out  duration-500 hover:scale-125 ">
-            <p className="caption text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
+            <p className=" caption text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
               Cicada
             </p>
           </p>
