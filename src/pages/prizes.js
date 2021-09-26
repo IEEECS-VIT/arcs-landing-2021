533cb09b8edbccaac3ef10720a8fc3874bc75ebd
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-09-26 13:32:00 +0530

    Rectangles added

--- src/pages/prizes.js
+++ src/pages/prizes.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../components/navbar";
+import Rect from "./../images/prizeRect.png";
 const Prizes = () => {
   const [isExpanded, toggleExpansion] = useState(false);
   var Blur = isExpanded ? "change-opacity" : "none";
@@ -7,10 +8,18 @@ const Prizes = () => {
     <div className="-mt-5">
       <Navbar isExpanded={isExpanded} toggleExpansion={toggleExpansion} />
       <div className={Blur}>
+        <img
+          src={Rect}
+          className="absolute invisible md:visible w-24 h-auto mt-10 mr-5"
+        />
+        <img
+          src={Rect}
+          className="absolute invisible md:visible w-24 h-auto mt-20 ml-5 right-0 transform rotate-180"
+        />
         <p className=" text-center font-bold text-4xl md:text-5xl font-mont">
           PRIZES
         </p>
-        <div className="justify-center mt-7 md:mt-32 flex flex-col md:flex-row">
+        <div className="justify-center mt-7 md:mt-32 flex flex-col md:flex-row ">
           <p className="md:-mr-16 z-10 circles prize3 transform ease-in-out  duration-500 hover:scale-125">
             <p className=" caption text-center mt-10 md:-mt-10 font-mont font-semibold text-2xl">
               Quiz
@@ -20,7 +29,7 @@ const Prizes = () => {
             <p className="z-50 md:pb-10 relative caption-2 text-center  font-mont font-semibold text-2xl">
               Competitive
             </p>
-            <p className=" z-20 circles prize1 transform   ease-in-out  duration-500 "></p>
+            <p className="z-20 circles prize1 transform   ease-in-out  duration-500 "></p>
           </div>
 
           <p className="md:-ml-16 z-10 circles  prize2  transform ease-in-out  duration-500 hover:scale-125 ">
