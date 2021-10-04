3cf67b2ab2d872fa789247e0fb4248088dc92318
[supraditya] <[aditya300100@gmail.com]>
2021-10-04 12:28:32 +0530

    Updated tailwind config for proper purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
