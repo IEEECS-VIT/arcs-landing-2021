04f727916913fbc7c1eabb7489ecc59603213649
pragati2526 <spragati126@gmail.com>
2021-10-12 20:03:47 +0530

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,17 @@ module.exports = {
         implementation: require("node-sass"),
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'React go',
+        short_name: 'Reactgo',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/images/arcs.png',
+      },
+    }
   ],
 };
